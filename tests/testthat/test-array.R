a6d9871b052ae47cfb30691a4d5ab63a4b4a4da2
Artur-man <artur-man@hotmail.com>
2025-03-18 20:52:05 +0100

    create more tests for createImgArray

---
+++ tests/testthat/test-array.R
@@ -0,0 +1,36 @@
+library(magick)
+
+img.file <- system.file("extdata", "bird.png", package = "ImageArray")
+
+test_that("magick input", {
+  
+  # create image
+  img <- magick::image_read(img.file)
+  
+  # create ImgArray
+  imgarray <- createImgArray(img, n.series = 2)
+  imgarray2 <- as.raster(imgarray, max.pixel.size = 300)
+  plot(imgarray2)
+  
+  # crop
+  imgarray_vis <- crop(imgarray, ind = list(100:200, 100:200))
+  imgarray_vis <- as.raster(imgarray_vis)
+  plot(imgarray_vis)
+  
+  # rotate 
+  lapply(c(0,90,180,270,360), function(x){
+    imgarray_vis <- rotate(imgarray, degree = x)
+    imgarray_vis <- as.raster(imgarray_vis)
+    plot(imgarray_vis)
+  })
+  expect_error(imgarray_vis <- rotate(imgarray, degree = 225))
+  
+  # flip/flop
+  imgarray_vis <- flip(imgarray)
+  imgarray_vis <- as.raster(imgarray_vis)
+  plot(imgarray_vis)
+  imgarray_vis <- flop(imgarray)
+  imgarray_vis <- as.raster(imgarray_vis)
+  plot(imgarray_vis)
+  
+})
