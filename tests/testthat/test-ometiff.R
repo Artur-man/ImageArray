bca5fb0c21a0bbc7c982312337802c634af2905d
Artur-man <artur-man@hotmail.com>
2025-04-07 22:08:29 +0200

    add new data

---
+++ tests/testthat/test-ometiff.R
@@ -0,0 +1,36 @@
+library(magick)
+skip_if_not_installed("ggplot2")
+library(ggplot2)
+
+# image file
+img.file <- system.file("extdata", "xy_12bit__plant.ome.tiff", package = "ImageArray")
+
+test_that("file input", {
+  
+  # create ImgArray
+  imgarray <- createImgArray(img.file, n.series = 2)
+  imgarray2 <- as.raster(imgarray, max.pixel.size = 300)
+  plot(imgarray2)
+  
+  # crop
+  imgarray_vis <- crop(imgarray, ind = list(100:200, 100:200))
+  imgarray_vis <- as.raster(imgarray_vis)
+  plot(imgarray_vis)
+  
+  # rotate 
+  lapply(c(0,90,180,270,360), function(x){
+    imgarray_vis <- rotate(imgarray, degree = x)
+    imgarray_vis <- as.raster(imgarray_vis)
+    plot(imgarray_vis)
+  })
+  expect_error(imgarray_vis <- rotate(imgarray, degree = 225))
+  
+  # flip/flop
+  imgarray_vis <- flip(imgarray)
+  imgarray_vis <- as.raster(imgarray_vis)
+  plot(imgarray_vis)
+  imgarray_vis <- flop(imgarray)
+  imgarray_vis <- as.raster(imgarray_vis)
+  plot(imgarray_vis)
+  
+})
