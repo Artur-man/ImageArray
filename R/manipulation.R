2e3435af59b89477c564a584e0e0f2be6af5e628
Artur-man <artur-man@hotmail.com>
2025-09-02 01:18:14 +0200

    set air

--- R/manipulation.R
+++ R/manipulation.R
@@ -1,136 +1,128 @@
 #' @describeIn ImgArray-methods rotate image array to 90, 180, 270 degrees
 #' @exportMethod rotate
-setMethod("rotate", 
-          signature = "ImgArray",
-          function(object, degrees){
-            
-            # validate rotation
-            if(!degrees %in% c(0,90,180,270,360)){
-              stop("Only rotations of 0,90,180,270,360 degrees are supported!")
-            }
-            
-            # aperm
-            if(degrees %in% c(90, 270)){
-              object <- aperm(object, perm = c(1,3,2)) 
-            }
-            
-            # flop
-            if(degrees %in% c(90,180)){
-              object <- flop(object)
-            }
-            
-            # flip
-            if(degrees %in% c(180, 270)){
-              object <- flip(object)
-            }
-            
-            # return
-            object
-          })
+setMethod("rotate", signature = "ImgArray", function(object, degrees) {
+  # validate rotation
+  if (!degrees %in% c(0, 90, 180, 270, 360)) {
+    stop("Only rotations of 0,90,180,270,360 degrees are supported!")
+  }
+
+  # aperm
+  if (degrees %in% c(90, 270)) {
+    object <- aperm(object, perm = c(1, 3, 2))
+  }
+
+  # flop
+  if (degrees %in% c(90, 180)) {
+    object <- flop(object)
+  }
+
+  # flip
+  if (degrees %in% c(180, 270)) {
+    object <- flip(object)
+  }
+
+  # return
+  object
+})
 
 #' @describeIn ImgArray-methods permute image
 #' @exportMethod aperm
-setMethod("aperm", 
-          signature = "ImgArray",
-          function(a, perm){
-            n.series <- length(a@series)
-            for(i in seq_len(n.series)){
-              a[[i]] <- aperm(a[[i]], perm = perm)
-            }
-            a
-          })
+setMethod("aperm", signature = "ImgArray", function(a, perm) {
+  n.series <- length(a@series)
+  for (i in seq_len(n.series)) {
+    a[[i]] <- aperm(a[[i]], perm = perm)
+  }
+  a
+})
 
 #' @describeIn ImgArray-methods negate image
 #' @exportMethod negate
-setMethod("negate", 
-          signature = "ImgArray",
-          function(object){
-            n.series <- length(object@series)
-            for(i in seq_len(n.series)){
-              object[[i]] <- 255L - object[[i]]
-            }
-            object
-          })
+setMethod("negate", signature = "ImgArray", function(object) {
+  n.series <- length(object@series)
+  for (i in seq_len(n.series)) {
+    object[[i]] <- 255L - object[[i]]
+  }
+  object
+})
 
 #' @describeIn ImgArray-methods modulate image
 #' @exportMethod modulate
-setMethod("modulate", 
-          signature = "ImgArray",
-          function(object, brightness){
-            if(brightness < 0)
-              stop("Brightness should be more than 0, typically more than 100")
-            dim_img <- dim(object[[1]])
-            # if(dim_img[1] > 1)
-            #   stop("Only the single channel images can be modulated!")
-            n.series <- length(object@series)
-            for(i in seq_len(n.series)){
-              tmp <- ceiling(object[[i]] * (brightness/100))
-              max <- if(type(object[[i]]) == "double") 1 else 255
-              tmp[tmp > max] <- max
-              if(max == 255) type(tmp) <- "integer"
-              object[[i]] <- tmp
-            }
-            object
-          })
+setMethod("modulate", signature = "ImgArray", function(object, brightness) {
+  if (brightness < 0) {
+    stop("Brightness should be more than 0, typically more than 100")
+  }
+  dim_img <- dim(object[[1]])
+  n.series <- length(object@series)
+  for (i in seq_len(n.series)) {
+    tmp <- ceiling(object[[i]] * (brightness / 100))
+    max <- if (type(object[[i]]) == "double") 1 else 255
+    tmp[tmp > max] <- max
+    if (max == 255) {
+      type(tmp) <- "integer"
+    }
+    object[[i]] <- tmp
+  }
+  object
+})
 
 #' @describeIn ImgArray-methods vertical flipping image
 #' @exportMethod flip
-setMethod("flip", 
-          signature = "ImgArray",
-          function(object){
-            n.series <- length(object@series)
-            for(i in seq_len(n.series)){
-              img <- object[[i]]
-              dim_img <- dim(img)
-              object[[i]] <- img[ , , dim_img[3]:1, drop = FALSE]
-            }
-            object
-          })
+setMethod("flip", signature = "ImgArray", function(object) {
+  n.series <- length(object@series)
+  for (i in seq_len(n.series)) {
+    img <- object[[i]]
+    dim_img <- dim(img)
+    object[[i]] <- img[,, dim_img[3]:1, drop = FALSE]
+  }
+  object
+})
 
 #' @describeIn ImgArray-methods horizontal flipping image
 #' @exportMethod flop
-setMethod("flop", 
-          signature = "ImgArray",
-          function(object){
-            n.series <- length(object@series)
-            for(i in seq_len(n.series)){
-              img <- object[[i]]
-              dim_img <- dim(img)
-              object[[i]] <- img[ , dim_img[2]:1, , drop = FALSE]
-            }
-            object
-          })
+setMethod("flop", signature = "ImgArray", function(object) {
+  n.series <- length(object@series)
+  for (i in seq_len(n.series)) {
+    img <- object[[i]]
+    dim_img <- dim(img)
+    object[[i]] <- img[, dim_img[2]:1, , drop = FALSE]
+  }
+  object
+})
 
 #' @describeIn ImgArray-methods cropping image
 #' @importFrom utils head tail
 #' @exportMethod crop
-setMethod("crop", 
-          signature = "ImgArray",
-          function(object, ind){
-            
-            # check ind
-            if(!is.list(ind))
-              stop("'ind' should be a list of integers")
-            if((length(dim(object[[1]])) - 1) != length(ind))
-              stop("'ind' should be a list of integers")
-            check_sequential <- all(vapply(ind, is.sequential, logical(1)))
-            if(!check_sequential)
-              stop("'ind' should be a list of sequantial integer 
-                   vectors (hence slice)")
-            
-            # crop all images
-            n.series <- length(object@series)
-            for(i in seq_len(n.series)){
-              img <- object[[i]]
-              dim_img <- dim(img)
-              cur_ind <- lapply(seq_len(length(ind)), function(j){
-                curind <- ind[[j]]
-                id <- c(floor(utils::head(curind,1)/(2^(i-1))), 
-                          ceiling(utils::tail(curind,1)/(2^(i-1))))
-                seq(max(id[1],1), min(id[2],dim_img[j+1]))
-              })
-              object[[i]] <- img[, cur_ind[[1]], cur_ind[[2]], drop = FALSE]
-            }
-            
-            object
-          })
+setMethod("crop", signature = "ImgArray", function(object, ind) {
+  # check ind
+  if (!is.list(ind)) {
+    stop("'ind' should be a list of integers")
+  }
+  if ((length(dim(object[[1]])) - 1) != length(ind)) {
+    stop("'ind' should be a list of integers")
+  }
+  check_sequential <- all(vapply(ind, is.sequential, logical(1)))
+  if (!check_sequential) {
+    stop(
+      "'ind' should be a list of sequantial integer 
+                   vectors (hence slice)"
+    )
+  }
+
+  # crop all images
+  n.series <- length(object@series)
+  for (i in seq_len(n.series)) {
+    img <- object[[i]]
+    dim_img <- dim(img)
+    cur_ind <- lapply(seq_len(length(ind)), function(j) {
+      curind <- ind[[j]]
+      id <- c(
+        floor(utils::head(curind, 1) / (2^(i - 1))),
+        ceiling(utils::tail(curind, 1) / (2^(i - 1)))
+      )
+      seq(max(id[1], 1), min(id[2], dim_img[j + 1]))
+    })
+    object[[i]] <- img[, cur_ind[[1]], cur_ind[[2]], drop = FALSE]
+  }
+
+  object
+})
--- R/utils.R
+++ R/utils.R
@@ -4,12 +4,9 @@
 #' @importFrom DelayedArray path
 #' @export
 #' @returns the path to ImgArray object store
-setMethod("path", 
-          signature = "ImgArray",
-          function(object){
-            DelayedArray::path(object[[1]])
-          }
-)
+setMethod("path", signature = "ImgArray", function(object) {
+  DelayedArray::path(object[[1]])
+})
 
 #' path of ImgArray image
 #'
@@ -19,53 +16,50 @@ setMethod("path",
 #' @export
 #' @return does not return a value, updates the path of the ImgArray object
 setReplaceMethod(
-  "path", 
+  "path",
   signature = "ImgArray",
-  function(object, value){
+  function(object, value) {
     n.series <- length(object)
-    for(i in seq_len(n.series)){
-      object[[i]] <- 
+    for (i in seq_len(n.series)) {
+      object[[i]] <-
         modify_seeds(
           object[[i]],
           function(x) {
             ind <- grepl("path", slotNames(x))
             path.name <- methods::slotNames(x)[ind]
             file_path <- methods::slot(x, name = path.name)
-            if(grepl(".zarr", file_path)){
-              name <- strsplit(file_path, 
-                               split = "\\.zarr")[[1]][2]
+            if (grepl(".zarr", file_path)) {
+              name <- strsplit(file_path, split = "\\.zarr")[[1]][2]
               value <- file.path(value, name)
-            } 
+            }
             methods::slot(x, name = path.name) <- value
             x
-          })
+          }
+        )
     }
     return(object)
   }
 )
 
 #' @noRd
-.isTRUEorFALSE <- function (x) {
+.isTRUEorFALSE <- function(x) {
   is.logical(x) && length(x) == 1L && !is.na(x)
 }
 
 #' @noRd
-.isSingleString <- function (x) {
+.isSingleString <- function(x) {
   is.character(x) && length(x) == 1L && !is.na(x)
 }
 
 #' modify_seeds
 #' @importFrom methods is
 #' @noRd
-.modify_seeds <- function (x, FUN, ...) 
-{
+.modify_seeds <- function(x, FUN, ...) {
   if (methods::is(x, "DelayedUnaryOp")) {
     x@seed <- modify_seeds(x@seed, FUN, ...)
-  }
-  else if (methods::is(x, "DelayedNaryOp")) {
+  } else if (methods::is(x, "DelayedNaryOp")) {
     x@seeds <- lapply(x@seeds, modify_seeds, FUN, ...)
-  }
-  else {
+  } else {
     x <- FUN(x, ...)
   }
   return(x)
@@ -73,7 +67,6 @@ setReplaceMethod(
 
 #' is.sequential
 #' @noRd
-is.sequential <- function(x){
+is.sequential <- function(x) {
   all(abs(diff(x)) == 1)
-}  
-
+}
