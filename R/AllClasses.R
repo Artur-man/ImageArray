4c344572d35a755e77cc5ab6f216707be72ada00
Artur-man <artur-man@hotmail.com>
2025-10-19 15:08:48 +0200

    remove commented out documentation

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,13 +1,3 @@
-# #' The ImgArray Class
-# #' 
-# #' A class for uni-layer and multi-layer DelayedArray-based images
-# #' 
-# #' @slot series a list of DelayedArray
-# #' 
-# #' @name ImgArray-class
-# #' @rdname ImgArray-class
-# #' @exportClass ImgArray
-# 
 .ImgArray <- setClass(
   Class = "ImgArray",
   slots = c(
@@ -15,8 +5,6 @@
   )
 )
 
-# #' @describeIn BFArray-class BFArraySeed class
-# #' @exportClass BFArraySeed BFArray
 .BFArraySeed <- setClass(
   "BFArraySeed",
   contains = "Array",
@@ -29,22 +17,6 @@
   )
 )
 
-# #' The BFArray Class
-# #' 
-# #' A class for image arrays read by RBioFormats
-# #' 
-# #' @slot seed DelayedArray seed
-# #' @slot filepath character, path to the image file
-# #' @slot series numeric, series ID of the pyramidal image
-# #' @slot resolution numeric, resolution ID of the pyramidal image
-# #' @slot shape numeric, shape of the image array
-# #' @slot type character, data type of the image array
-# #' 
-# #' @name BFArray-class
-# #' @rdname BFArray-class
-# #' 
-# #' @usage BFArray(arg1, arg2 = default, ...)
-# #' @exportClass BFArray
 .BFArray <- setClass(
   Class = "BFArray",
   contains = c("DelayedArray"),
